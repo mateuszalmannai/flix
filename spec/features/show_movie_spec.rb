5269106c48189746e38a3b9a792164061bf33408
mateusz <mateuszalmannai@googlemail.com>
2015-07-25 15:22:00 +0200

    spec out page navigation

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -4,11 +4,7 @@ describe "Viewing an individual movie" do
   it "shows the movie's details" do
 
   # ARRANGE
-  movie = Movie.create(title: "Iron Man",
-                       rating: "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armoured suit to fight the throes of evil",
-                       released_on: "2008-05-02")
+  movie = Movie.create(movie_attributes(total_gross: 300000000.00))
 
   # ACT
   visit movie_url(movie)
@@ -16,7 +12,7 @@ describe "Viewing an individual movie" do
 
   expect(page).to have_text(movie.title)
   expect(page).to have_text(movie.rating)
-  expect(page).to have_text("$318,412,101.00")
+  expect(page).to have_text("$300,000,000.00")
   expect(page).to have_text(movie.description)
   expect(page).to have_text(movie.released_on)
   end
---
+++ spec/support/attributes.rb
@@ -0,0 +1,15 @@
+# Calling this method without any parameters simply returns the hash of attribute names and values.
+# The default hash parameters allows any of the attribute values to be overridden
+# When you call movie_attributes with a hash of attributes, that hash will get merged with the
+# existing attributes hash, i.e. to change the total_gross attribute value:
+# movie_attributes(total_gross: 6000000.00)
+#
+def movie_attributes(overrides = {})
+  {
+    title: "Iron Man",
+    rating: "PG-13",
+    total_gross: 318412101.00,
+    description: "Tony Stark builds an armored suit to fight the throes of evil",
+    released_on: "2008-05-02"
+  }.merge(overrides)
+end
