a25a1e4717751ac6e8ceb394eaa59eefd88d50d5
mateusz <mateuszalmannai@googlemail.com>
2015-08-01 20:55:01 +0200

    remove template duplication with partial and add cancel link

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -1,17 +1,7 @@
 describe 'Creating a new movie' do
 
   it "should save the movie and shows the movie's details" do
-    visit movies_url
-
-    click_link 'Add New Movie'
-
-    expect(current_path).to eq(new_movie_path)
-
-    fill_in "Title", with: "New Movie Title"
-    fill_in "Description", with: "Superheroes saving the world from villains"
-    fill_in "Rating", with: "PG-13"
-    fill_in "Total gross", with: "75000000"
-    select (Time.now.year - 1).to_s, :from => "movie_released_on_1i" # must be in past
+    setup_new_movie
 
     # If you're taking advantage of the HTML 5 date field in Chrome,
     # you'll need to use 'fill_in' rather than 'select'
@@ -22,4 +12,31 @@ describe 'Creating a new movie' do
 
     expect(page).to have_text('New Movie Title')
   end
+
+  it "does not save the movie and goes to listing page when 'Cancel' is pressed" do
+    setup_new_movie
+
+    click_link "Cancel"
+
+    expect(current_path).to eq(movies_path)
+
+    expect(page).to_not have_text('New Movie Title')
+  end
 end
+
+private
+def setup_new_movie
+  visit movies_url
+
+  click_link 'Add New Movie'
+
+  expect(current_path).to eq(new_movie_path)
+
+  fill_in "Title", with: "New Movie Title"
+  fill_in "Description", with: "Superheroes saving the world from villains"
+  fill_in "Rating", with: "PG-13"
+  fill_in "Total gross", with: "75000000"
+  select (Time.now.year - 1).to_s, :from => "movie_released_on_1i" # must be in past
+
+end
+
--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -1,11 +1,16 @@
+def setup_movie_to_edit
+  movie = Movie.create(movie_attributes)
+
+  visit movie_url(movie)
+
+  click_link 'Edit'
+  movie
+end
+
 describe "Editing a movie" do
 
   it "updates the movie and shows the movie's updated details" do
-    movie = Movie.create(movie_attributes)
-
-    visit movie_url(movie)
-
-    click_link 'Edit'
+    movie = setup_movie_to_edit
 
     expect(current_path).to eq(edit_movie_path(movie))
 
@@ -20,4 +25,19 @@ describe "Editing a movie" do
     expect(page).to have_text('Updated Movie Title')
   end
 
+  it "does not update the movie and goes to the listing page when 'Cancel' is pressed" do
+    movie = setup_movie_to_edit
+
+    expect(current_path).to eq(edit_movie_path(movie))
+
+    expect(find_field('movie_title').value).to eq(movie.title)
+
+    fill_in "Title", with: "Updated Movie Title"
+
+    click_link "Cancel"
+
+    expect(current_path).to eq(movies_path)
+
+    expect(page).to_not have_text('Updated Event Name')
+  end
 end
