8cbc5b3d60e1a9b62182d2fb5deec733e6903d39
mateusz <mateuszalmannai@googlemail.com>
2015-08-08 18:29:14 +0200

    add query methods to movie

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -43,4 +43,13 @@ describe "Viewing the list of movies" do
 
     expect(page).to have_text("The Pragmatic Studio")
   end
+
+  it "does not show a movie that hasn't yet been released" do
+    movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+    visit movies_path
+
+    expect(page).not_to have_text(movie.title)
+  end
+
 end
